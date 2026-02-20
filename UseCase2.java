fccd0b08e37a595162fbdc6a0449eb7599bd08c6
vishwajeet-reddy <vd4278@srmist.edu.in>
2026-02-21 03:02:58 +0530

    uc2

---
+++ UseCase2.java
@@ -0,0 +1,12 @@
+public class UseCase2 {
+    public static void main(String[] args) {
+        // uc2:
+        System.out.println("   ***       ***     ******    *****  ");
+        System.out.println(" **   **   **   **   **   **  **    * ");
+        System.out.println(" *     *   *     *   **   **  **      ");
+        System.out.println(" *     *   *     *   ******    ***    ");
+        System.out.println(" *     *   *     *   **           **  ");
+        System.out.println(" **   **   **   **   **      *    **  ");
+        System.out.println("   ***       ***     **       *****   ");
+    }
+}
