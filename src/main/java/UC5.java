cb26cf486a92b8b0e1b41c9586e380a5a288ea56
vishwajeet-reddy <vd4278@srmist.edu.in>
2026-04-02 00:21:16 +0530

    UC5 added

---
+++ src/main/java/UC5.java
@@ -0,0 +1,27 @@
+public class UC5 {
+    public static void main(String[] args) {
+
+        String name;
+
+        // If no arguments → default
+        if (args.length == 0) {
+            name = "World";
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+
+            // Enhanced for loop
+            for (String n : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(n);
+                first = false;
+            }
+
+            name = nameBuilder.toString();
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
